38734a89d88d72c87a206cf4fa7e75597903b2af
ussiiefsayed-ctrl <ussiiefsayed@gmail.com>
2026-05-04 16:14:02 +0300

    Add files via upload

---
+++ Libraries/DS/clsMyStackArr.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "clsMyQueueArr.h"
+
+template <class T>
+class clsMyStackArr :public clsMyQueueArr <T>
+{
+public:
+
+	void push(T Item)
+	{
+			clsMyQueueArr <T>::_MyList.InsertAtBeginning(Item);
+	}
+
+	T Top()
+	{
+		return clsMyQueueArr <T>::front();
+	}
+
+	T Bottom()
+	{
+		return clsMyQueueArr <T>::back();
+	}
+
+
+};
+
